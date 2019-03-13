b230b6fbcee41dcf843f428e76c11534911c028e
arihane mariano <arihane.mariano@conexaolocal.com.br>
2019-03-13 14:07:24 -0300

    Dividindo em modulos

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 
 from tkinter import ttk
+from app2 import *
 
 LARGE_FONT= ("Verdana", 12)
 
@@ -38,63 +39,7 @@ class SeaofBTCapp(tk.Tk):
         print ('oi')
 
         
-class StartPage(tk.Frame):
 
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self,parent)
-
-        logo = tk.PhotoImage(file='/home/usuario/Documentos/python/app/teste.png')
-        
-        label = ttk.Label(self, text="Start Page", font=LARGE_FONT,image=logo)
-        label.image = logo
-        label.pack(pady=10,padx=10)
-
-        #button = ttk.Button(self, text="Visit Page 1",
-                            #command=lambda: controller.show_frame(PageOne))
-
-        button = ttk.Button(self, text="Visit Page 1",
-                            command=lambda: controller.teste())
-        button.pack()
-
-        #button2 = ttk.Button(self, text="Visit Page 2",
-                            #command=lambda: controller.show_frame(PageTwo))
-        button2 = ttk.Button(self, text="Visit Page 2",
-                            command=lambda: controller.teste())
-        
-        button2.pack()
-
-
-class PageOne(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Page One!!!", font=LARGE_FONT)
-        
-        label.pack(pady=10,padx=10)
-
-        button1 = ttk.Button(self, text="Back to Home",
-                            command=lambda: controller.show_frame(StartPage))
-        button1.pack()
-
-        button2 = ttk.Button(self, text="Page Two",
-                            command=lambda: controller.show_frame(PageTwo))
-        button2.pack()
-
-
-class PageTwo(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Page Two!!!", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
-
-        button1 = ttk.Button(self, text="Back to Home",
-                            command=lambda: controller.show_frame(StartPage))
-        button1.pack()
-
-        button2 = ttk.Button(self, text="Page One",
-                            command=lambda: controller.show_frame(PageOne))
-        button2.pack()
         
 
 
